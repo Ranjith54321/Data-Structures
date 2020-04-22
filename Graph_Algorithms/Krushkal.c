767f914322db385c518af8797a7e9675390e6c3c
Ranjith_dot <39690967+Ranjith54321@users.noreply.github.com>
2020-04-22 12:16:16 +0530

    Create Krushkal.c

---
+++ Graph_Algorithms/Krushkal.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+// a structure to represent a weighted edge in graph
+struct Edge
+{
+    int src, dest, weight;
+};
+ 
+// a structure to represent a connected, undirected and weighted graph
+struct Graph
+{
+    // V-> Number of vertices, E-> Number of edges
+    int V, E;
+ 
+    // graph is represented as an array of edges. Since the graph is
+    // undirected, the edge from src to dest is also edge from dest
+    // to src. Both are counted as 1 edge here.
+    struct Edge* edge;
+};
+ 
+// Creates a graph with V vertices and E edges
+struct Graph* createGraph(int V, int E)
+{
+    struct Graph* graph = (struct Graph*) malloc( sizeof(struct Graph) );
+    graph->V = V;
+    graph->E = E;
+ 
+    graph->edge = (struct Edge*) malloc( graph->E * sizeof( struct Edge ) );
+ 
+    return graph;
+}
+ 
+// A structure to represent a subset for union-find
+struct subset
+{
+    int parent;
+    int rank;
+};
+ 
+// A utility function to find set of an element i
+// (uses path compression technique)
+int find(struct subset subsets[], int i)
+{
+    // find root and make root as parent of i (path compression)
+    if (subsets[i].parent == i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+ 
+    return subsets[i].parent;
+}
+ 
+// A function that does union of two sets of x and y
+// (uses union by rank)
+void Union(struct subset subsets[], int x, int y)
+{
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+ 
+    // Attach smaller rank tree under root of high rank tree
+    // (Union by Rank)
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+ 
+    // If ranks are same, then make one as root and increment
+    // its rank by one
+    else
+    {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+ 
+// Compare two edges according to their weights.
+// Used in qsort() for sorting an array of edges
+int myComp(const void* a, const void* b)
+{
+    struct Edge* a1 = (struct Edge*)a;
+    struct Edge* b1 = (struct Edge*)b;
+    return a1->weight > b1->weight;
+}
+ 
+// The main function to construct MST using Kruskal's algorithm
+void KruskalMST(struct Graph* graph)
+{
+    int V = graph->V;
+    int E = graph->E;
+    int i,e;
+    struct Edge result[V];
+    
+    qsort(graph->edge,E,sizeof(graph->edge[0]),myComp); // qsort in built function 
+    
+    struct subset *st = (struct subset*)malloc(V*sizeof(struct subset));
+    
+    for(i=0;i<V;i++){
+        st[i].parent = i;
+        st[i].rank = 0;
+    }
+    i=0;
+    e=0;
+    while(e<V-1 && i<E){
+        
+        struct Edge next_edge = graph->edge[i++]; // 
+        int x = find(st,next_edge .src);
+        int y = find(st,next_edge .dest);
+        
+        if(x!=y){
+            result[e++] = next_edge;
+            Union(st,x,y);
+        }
+    }
+ 
+    // print the contents of result[] to display the built MST
+    printf("Following are the edges in the constructed MST\n");
+    for (i = 0; i < e; ++i)
+        printf("Edge %d and Edge %d with distance %d\n", result[i].src, result[i].dest, result[i].weight);
+    return;
+}
+ 
+// Driver program to test above functions
+int main()
+{
+    int V;
+    int E;
+    int i;
+    printf("Enter the number of vertices\n");
+	scanf("%d",&V);
+	printf("Enter the number of edges\n");
+	scanf("%d",&E);
+    struct Graph* graph = createGraph(V, E);
+ 
+	for(i=0;i<E;i++){
+		printf("Enter source, destination and weight of edge %d\n",i+1);
+		scanf("%d",&(graph->edge[i].src));
+		scanf("%d",&(graph->edge[i].dest));
+		scanf("%d",&(graph->edge[i].weight));
+	}
+	
+    KruskalMST(graph);
+    return 0;
+}
