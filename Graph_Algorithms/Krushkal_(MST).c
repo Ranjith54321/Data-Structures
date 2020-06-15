cae80f0c15ff3fcb9d9a6edc166565e3a9a546ba
Ranjith S <39690967+Ranjith54321@users.noreply.github.com>
2020-06-15 15:44:12 +0530

    Update Krushkal_(MST).c

--- Graph_Algorithms/Krushkal_(MST).c
+++ Graph_Algorithms/Krushkal_(MST).c
@@ -44,7 +44,7 @@ struct subset
 int find(struct subset subsets[], int i)
 {
     // find root and make root as parent of i (path compression)
-    if (subsets[i].parent == i)
+    if (subsets[i].parent != i)
         subsets[i].parent = find(subsets, subsets[i].parent);
  
     return subsets[i].parent;
